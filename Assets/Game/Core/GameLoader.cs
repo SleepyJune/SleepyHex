e8d6320e1591e2ae344576c63656f4c0b60fed26
notequals <hshsiao63@gmail.com>
2018-04-30 02:07:15 -0700

    Update -Added loading screen

--- Assets/Game/Core/GameLoader.cs
+++ Assets/Game/Core/GameLoader.cs
@@ -42,41 +42,6 @@ public class GameLoader : MonoBehaviour
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
         operation.allowSceneActivation = false;
 
-        for(int i=0;i<100;i++)
-        {
-            if (anim)
-            {
-                var value = Mathf.Clamp01(i/100.0f); ;
-
-                anim.SetFloat("Progress", value);
-            }
-
-            yield return new WaitForSeconds(0.01f);
-        }
-
-        if (operation.progress >= 0.9f && !operation.allowSceneActivation)
-        {
-            operation.allowSceneActivation = true;
-        }
-
-        //if (LoadGroup) LoadGroup.SetActive(false);
-        if (LoadingCallBack != null)
-        {
-            LoadingCallBack();
-        }
-    }
-
-    IEnumerator AsyncLoad2(string sceneName)
-    {
-        /*if (LoadGroup)
-        {
-            LoadGroup.SetActive(true);
-        }
-        yield return new WaitForSeconds(0.2f);*/
-
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-        operation.allowSceneActivation = false;
-
         while (!operation.isDone)
         {
             if (anim)
@@ -90,7 +55,7 @@ public class GameLoader : MonoBehaviour
             {
                 operation.allowSceneActivation = true;
             }
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.01f);
         }
 
         //if (LoadGroup) LoadGroup.SetActive(false);
