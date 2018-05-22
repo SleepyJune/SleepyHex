508fc82a61c800bade9b5a8cd688a949724245c8
notequals <hshsiao63@gmail.com>
2018-05-21 22:58:34 -0700

    Update -version 1.9

--- Assets/Game/Core/AdMobManager.cs
+++ Assets/Game/Core/AdMobManager.cs
@@ -27,6 +27,11 @@ public class AdMobManager : MonoBehaviour
 
         DontDestroyOnLoad(this);
 
+        if (PlayerPrefs.GetInt("PlayedTutorial", 0) == 0) //don't block tutorial
+        {
+            return;
+        }
+
 
 #if UNITY_ANDROID
         string appId = "ca-app-pub-7441401095869420~6580101511";
@@ -40,16 +45,13 @@ public class AdMobManager : MonoBehaviour
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize(appId);
 
-        if (PlayerPrefs.GetInt("PlayedTutorial", 0) > 0) //don't block tutorial
-        {
-            RequestBanner();
-        }
+        RequestBanner();
     }
 
     private void RequestBanner()
     {
 #if UNITY_ANDROID
-        string adUnitId = "ca-app-pub-7441401095869420/9429217018";
+        string adUnitId = "ca-app-pub-7441401095869420/8634562987";
         //string adUnitId = "ca-app-pub-3940256099942544/6300978111"; //for test
 #elif UNITY_IPHONE
                     string adUnitId = "ca-app-pub-7441401095869420/9429217018";
@@ -60,11 +62,54 @@ public class AdMobManager : MonoBehaviour
         // Create a 320x50 banner at the top of the screen.
         bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
 
+        /*
+        // Called when an ad request has successfully loaded.
+        bannerView.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        bannerView.OnAdOpening += HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        bannerView.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        bannerView.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+        */
+
         // Create an empty ad request.
-        AdRequest request = new AdRequest.Builder().Build();
+        AdRequest request 
+            = new AdRequest.Builder()        
+            .Build(); //.AddTestDevice("ECCCC4AB628B5A5E1FFB38969709E24D")
 
         // Load the banner with the request.
         bannerView.LoadAd(request);
     }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+
+
 }
 
