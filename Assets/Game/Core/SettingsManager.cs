227b1c49ed0f3c9d6ffb2272749b4b9914fcba31
notequals <hshsiao63@gmail.com>
2018-07-24 21:52:53 -0700

    Update -Added saving mute sound preference -Merged sound and music together

--- Assets/Game/Core/SettingsManager.cs
+++ Assets/Game/Core/SettingsManager.cs
@@ -4,19 +4,49 @@ using System.Linq;
 using System.Text;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
     public Transform parent;
+
+    public Toggle muteMusicToggle;
+
+    void Awake()
+    {
+        var window = GetComponent<DialogWindow>();
+        if (window)
+        {
+            window.Close();
+        }
+
+        var muteMusicSetting = GetMuteMusicSetting();
+
+        MuteMusic(muteMusicSetting);
+
+        if (muteMusicToggle)
+        {
+            muteMusicToggle.isOn = muteMusicSetting;
+        }
+    }
 
     public void Back()
     {
         Destroy(parent.gameObject);
     }
 
+    public bool GetMuteMusicSetting()
+    {
+        return PlayerPrefs.GetInt("MuteMusicSetting", 0) == 1;
+    }
+
     public void MuteMusic(bool mute)
     {
-        SoundManager.instance.musicSource.mute = mute;
+        //SoundManager.instance.musicSource.mute = mute;
+
+        AudioListener.volume = mute ? 0 : 1;
+
+        PlayerPrefs.SetInt("MuteMusicSetting", Convert.ToInt32(mute));
     }
 
     public void MuteSound(bool mute)
